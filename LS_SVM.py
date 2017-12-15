7a8052b7b7e70f6cdfe1b9cac7922af8eddc7950
GernandtFabian <34569953+GernandtFabian@users.noreply.github.com>
2017-12-15 16:34:30 +0100

    Add files via upload

--- LS_SVM.py
+++ LS_SVM.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import linalg
+from scipy import optimize as opt
 import matplotlib.pyplot as plt
 
 
@@ -13,6 +14,8 @@ class Kernel:
         }.get(kern, self.linear)
         self.param = param
         self.hyperparam = hyperparam
+        self.omega = np.array([])
+        self.eig = np.array([])
 
     def linear(self, u, v):
         return np.inner(u, v)
@@ -52,26 +55,62 @@ def decision(a, x, y, v, kern):
     return np.dot(np.multiply(y, a[1:]), kern.kernel(x, v)) + a[0]
 
 
+def gesamt(data_dict, kern):
+    data = np.concatenate((data_dict[1], data_dict[-1]), axis=0)
+    data_y = np.append(np.ones([np.size(data_dict[1], axis=0), 1]), -np.ones([np.size(data_dict[-1], axis=0), 1]))
+    size_x = np.size(data_y)
+
+    def centering(mat):
+        M = np.eye(size_x) + np.multiply(1.0/size_x, np.ones([size_x, size_x]))
+        return np.dot(M, np.dot(mat, M))
+
+    for i, k in enumerate(kern):
+        k.omega = k.kernel(data, data)
+        k.eig, v = linalg.eig(centering(k.omega))
+
+    alpha = solve(data_dict, kern, plot=True)
+    hyper_map = solve2(alpha, kern)
+    print(hyper_map)
+
+
+def solve2(alpha, kern):
+    sol = np.array([])
+    for i, k in enumerate(kern):
+        omega = k.omega
+        eig = k.eig
+        n_eff = np.size(eig)
+        n = np.size(omega, axis=0)
+        print(alpha[i], omega)
+
+        def fun(hyper):
+            h1 = hyper[0] / 2.0 * np.dot(alpha[i, 1:], np.dot(omega, alpha[i, 1:]))
+            h2 = hyper[1] / 2.0 * (k.hyperparam[0] / k.hyperparam[1]) ** 2 * np.dot(alpha[i, 1:], alpha[i, 1:])
+            h3 = 1.0 / 2.0 * np.sum(np.log(np.add(hyper[0], np.multiply(hyper[1], eig))))
+            h4 = - n_eff / 2.0 * np.log(hyper[0]) + (n - 1) / 2.0 * np.log(hyper[1])
+            return np.sum([h1, h2, h3, h4])
+
+        # res = opt.fmin_cg(fun, np.array([k.hyperparam[0], k.hyperparam[1]]), full_output=False)
+        res = opt.minimize(fun, np.array([k.hyperparam[0], k.hyperparam[1]]), method='BFGS', jac=False)
+        sol = np.reshape(np.append(sol, res.x), (i + 1, 2))
+    return sol
+
+
 def solve(data_dict, kern, plot=False):
     data = np.concatenate((data_dict[1], data_dict[-1]), axis=0)
     data_y = np.append(np.ones([np.size(data_dict[1], axis=0), 1]), -np.ones([np.size(data_dict[-1], axis=0), 1]))
     size_x = np.size(data_y)
 
     sol = np.array([])
-    e = np.array([])
     for i, k in enumerate(kern):
-        gamma = k.hyperparam[0] / k.hyperparam[1]
+        gamma = k.hyperparam[1] / k.hyperparam[0]
         h1 = np.append([0], data_y)
-        omega = np.multiply(k.kernel(data, data), np.outer(data_y, data_y)) \
+        omega_ = np.multiply(k.omega, np.outer(data_y, data_y)) \
                 + 1 / gamma * np.eye(size_x)
-        h2 = np.concatenate(([data_y], omega)).T
+        h2 = np.concatenate(([data_y], omega_)).T
 
         A = np.append([h1], h2, axis=0)
         b = np.append([0], np.ones([1, size_x]))
         alpha = linalg.solve(A, b)
-
-        eig, vs = linalg.eig(omega)
-        print(eig)
 
         sol = np.reshape(np.append(sol, alpha), (i + 1, size_x + 1))
 
--- Testing.py
+++ Testing.py
@@ -2,19 +2,25 @@ import numpy as np
 import LS_SVM
 
 if __name__ == '__main__':
-    data_dict = {-1: np.array([[1, 2],
-                               [1, 1],
-                               [2, 1],
-                               [3, 4]]),
-                 1: np.array([[2, -3],
-                              [0, -3],
-                              [1, -1],
-                              [1.5, -2],
-                              [4, 2]])}
+    data_dict = {1: np.array([[1, 2],
+                              [1, 1],
+                              [2, 1],
+                              [3, 4]]),
+                 -1: np.array([[2, -3],
+                               [0, -3],
+                               [1, -1],
+                               [1.5, -2],
+                               [4, 2]])}
+    '''
+    data_dict = {1: np.array([[0, 0],
+                               [1, 0]]),
+                 -1: np.array([[0, 1],
+                              [1, 2]])}
+    '''
 
     k1 = LS_SVM.Kernel('lin', param=[None, None], hyperparam=[1, 3])
     k2 = LS_SVM.Kernel('pol', param=[1, 3], hyperparam=[4, 1])
     k3 = LS_SVM.Kernel('rad', param=[10, None], hyperparam=[1, 3])
     k4 = LS_SVM.Kernel('sig', param=[0.1, 1], hyperparam=[1, 1])
 
-    alpha = LS_SVM.solve(data_dict, [k1, k2, k3, k4], plot=True)
+    alpha = LS_SVM.gesamt(data_dict, [k1])
