db869aba9b689b04a73d9848d7048f9392061609
GernandtFabian <34569953+GernandtFabian@users.noreply.github.com>
2017-12-16 15:24:45 +0100

    Add files via upload

--- LS_SVM.py
+++ LS_SVM.py
@@ -61,38 +61,72 @@ def gesamt(data_dict, kern):
     size_x = np.size(data_y)
 
     def centering(mat):
-        M = np.eye(size_x) + np.multiply(1.0/size_x, np.ones([size_x, size_x]))
+        M = np.eye(size_x) + np.multiply(1.0 / size_x, np.ones([size_x, size_x]))
         return np.dot(M, np.dot(mat, M))
 
     for i, k in enumerate(kern):
         k.omega = k.kernel(data, data)
-        k.eig, v = linalg.eig(centering(k.omega))
+        eig, v = linalg.eig(centering(k.omega))
+        j = 0
+        for e in eig:
+            if e <= 1e-6:
+                eig = np.delete(eig, j)
+            else:
+                j += 1
+        k.eig = eig
 
-    alpha = solve(data_dict, kern, plot=True)
-    hyper_map = solve2(alpha, kern)
-    print(hyper_map)
+    ker = kern
+    while np.size(ker) > 1:
+        alpha = solve(data_dict, ker, plot=True)
+        solve2(alpha, ker)
+        ker = solve3(ker)
+    solve(data_dict, ker, plot=True)
+    pass
+
+
+def solve3(kern):
+    min = np.inf
+    min_i = 0
+    for i, k in enumerate(kern):
+        n_eff = np.size(k.eig)
+        n = np.size(k.omega, axis=0)
+        gamma_eff = 1 + np.sum(np.divide(np.multiply(k.hyperparam[1], k.eig),
+                                         (np.add(k.hyperparam[0], np.multiply(k.hyperparam[1], k.eig)))))
+        evi = np.sqrt((k.hyperparam[0] ** n_eff * k.hyperparam[1] ** (n - 1)) /
+                      ((gamma_eff - 1) * (n - gamma_eff)) * np.prod(np.add(k.hyperparam[0],
+                                                                           np.multiply(k.hyperparam[1], k.eig))))
+        if evi < min:
+            min = evi
+            min_i = i
+    if np.size(kern) > 1:
+        ker = np.delete(kern, min_i)
+    else:
+        ker = kern
+    return ker
 
 
 def solve2(alpha, kern):
+    cap = 1e-9
     sol = np.array([])
     for i, k in enumerate(kern):
         omega = k.omega
         eig = k.eig
         n_eff = np.size(eig)
         n = np.size(omega, axis=0)
-        print(alpha[i], omega)
 
         def fun(hyper):
             h1 = hyper[0] / 2.0 * np.dot(alpha[i, 1:], np.dot(omega, alpha[i, 1:]))
             h2 = hyper[1] / 2.0 * (k.hyperparam[0] / k.hyperparam[1]) ** 2 * np.dot(alpha[i, 1:], alpha[i, 1:])
             h3 = 1.0 / 2.0 * np.sum(np.log(np.add(hyper[0], np.multiply(hyper[1], eig))))
-            h4 = - n_eff / 2.0 * np.log(hyper[0]) + (n - 1) / 2.0 * np.log(hyper[1])
+            h4 = - n_eff / 2.0 * np.log(hyper[0]) - (n - 1) / 2.0 * np.log(hyper[1])
             return np.sum([h1, h2, h3, h4])
 
         # res = opt.fmin_cg(fun, np.array([k.hyperparam[0], k.hyperparam[1]]), full_output=False)
-        res = opt.minimize(fun, np.array([k.hyperparam[0], k.hyperparam[1]]), method='BFGS', jac=False)
-        sol = np.reshape(np.append(sol, res.x), (i + 1, 2))
-    return sol
+        res = opt.fmin(fun, np.array([k.hyperparam[0], k.hyperparam[1]]), disp=False)
+        res = np.array([np.max([res[0], cap]), np.max([res[1], cap])])
+        sol = np.reshape(np.append(sol, res), (i + 1, 2))
+        k.hyperparam = sol[i]
+    pass
 
 
 def solve(data_dict, kern, plot=False):
@@ -105,7 +139,7 @@ def solve(data_dict, kern, plot=False):
         gamma = k.hyperparam[1] / k.hyperparam[0]
         h1 = np.append([0], data_y)
         omega_ = np.multiply(k.omega, np.outer(data_y, data_y)) \
-                + 1 / gamma * np.eye(size_x)
+                 + 1 / gamma * np.eye(size_x)
         h2 = np.concatenate(([data_y], omega_)).T
 
         A = np.append([h1], h2, axis=0)
@@ -119,8 +153,8 @@ def solve(data_dict, kern, plot=False):
             plt.scatter(data_dict[1][:, 0], data_dict[1][:, 1], color='b')
             plt.scatter(data_dict[-1][:, 0], data_dict[-1][:, 1], color='r')
 
-            x = np.arange(np.min(data[:, 0]) - 4, np.max(data[:, 0]) + 4, 0.5)
-            y = np.arange(np.min(data[:, 1]) - 4, np.max(data[:, 1]) + 4, 0.5)
+            x = np.arange(np.min(data[:, 0]) - 10, np.max(data[:, 0]) + 10, 0.25)
+            y = np.arange(np.min(data[:, 1]) - 10, np.max(data[:, 1]) + 10, 0.25)
 
             X, Y = np.meshgrid(x, y)
 
--- Testing.py
+++ Testing.py
@@ -1,26 +1,45 @@
+import warnings
 import numpy as np
 import LS_SVM
+from sklearn import datasets
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+
+X, y = datasets.make_classification(n_samples=50, n_features=2, n_informative=2, n_redundant=0, n_classes=2,
+                                    random_state=7, class_sep=1.5, flip_y=0.1)
 
 if __name__ == '__main__':
     data_dict = {1: np.array([[1, 2],
                               [1, 1],
-                              [2, 1],
-                              [3, 4]]),
+                              [3, 4],
+                              [4, 2],
+                              [2, 1]]),
                  -1: np.array([[2, -3],
                                [0, -3],
                                [1, -1],
-                               [1.5, -2],
-                               [4, 2]])}
+                               [1.5, -2]])}
     '''
     data_dict = {1: np.array([[0, 0],
-                               [1, 0]]),
+                              [1, 0]]),
                  -1: np.array([[0, 1],
                               [1, 2]])}
     '''
 
-    k1 = LS_SVM.Kernel('lin', param=[None, None], hyperparam=[1, 3])
-    k2 = LS_SVM.Kernel('pol', param=[1, 3], hyperparam=[4, 1])
-    k3 = LS_SVM.Kernel('rad', param=[10, None], hyperparam=[1, 3])
-    k4 = LS_SVM.Kernel('sig', param=[0.1, 1], hyperparam=[1, 1])
+    k1 = LS_SVM.Kernel('lin', param=[None, None], hyperparam=[1, 1])
+    k2 = LS_SVM.Kernel('pol', param=[1, 2], hyperparam=[1, 1])
+    k3 = LS_SVM.Kernel('rad', param=[10, None], hyperparam=[1, 1])
+    k4 = LS_SVM.Kernel('sig', param=[0.1, 10], hyperparam=[1, 3])
 
-    alpha = LS_SVM.gesamt(data_dict, [k1])
+    M1 = np.array([])
+    M2 = np.array([])
+    j = 1
+    k = 1
+    for i, item in enumerate(X):
+        if y[i] == 1:
+            M1 = np.reshape(np.append(M1, item), (j, 2))
+            j += 1
+        else:
+            M2 = np.reshape(np.append(M2, item), (k, 2))
+            k += 1
+    data_dict_new = {1: M1, -1: M2}
+    LS_SVM.gesamt(data_dict_new, [k1, k2, k3, k4])
